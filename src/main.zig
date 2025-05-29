b8dcca4f78042564fbdcd10d0c0114575b4e8832
Traxar <trxr2a@gmail.com>
2025-05-29 09:05:38 +0200

    bump to v0.0.3 release

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const assert = std.debug.assert;
 const spade = @import("spade");
 
-const T = spade.tensor.Type(f32); //tensor base type, supports arbitrary element types
+const T = spade.Tensor(usize, f32); //tensor base type, supports arbitrary element types
 const V = T.Dense(&.{0}); //vector
 const Mcol = T.Dense(&.{ 0, 1 }); //column major matrix
 const Mrow = T.Dense(&.{ 1, 0 }); //row major matrix
